93f9040b541e58fd766d86effc9073ab67b68073
HuanLuo06 <156331366+HuanLuo06@users.noreply.github.com>
2024-07-18 01:33:26 -0400

    Update app.r

--- lm/app.r
+++ lm/app.r
@@ -9,111 +9,115 @@
 
 library(shiny)
 
-# Define UI for application that draws a histogram
+# Define UI for application that draws a scatter plot and linear model
 ui <- fluidPage(
+  
+  # Application title
+  titlePanel("Scatter Plot and Linear Model"),
+  
+  # Sidebar with file input and action button
+  sidebarLayout(
+    sidebarPanel(
+      
+      # Input: Select a file ----
+      fileInput("file1", "Choose CSV File",
+                multiple = FALSE,
+                accept = c("text/csv",
+                           "text/comma-separated-values,text/plain",
+                           ".csv")),
+      
+      # Horizontal line ----
+      tags$hr(),
+      
+      # Input: Checkbox if file has header ----
+      checkboxInput("header", "Header", TRUE),
+      
+      # Input: Select separator ----
+      radioButtons("sep", "Separator",
+                   choices = c(Comma = ",",
+                               Semicolon = ";",
+                               Tab = "\t"),
+                   selected = ","),
+      
+      # Input: Select quotes ----
+      radioButtons("quote", "Quote",
+                   choices = c(None = "",
+                               "Double Quote" = '"',
+                               "Single Quote" = "'"),
+                   selected = '"'),
+      
+      # Horizontal line ----
+      tags$hr(),
+      
+      # Input: Display head or all ----
+      radioButtons("disp", "Display",
+                   choices = c(Head = "head",
+                               All = "all"),
+                   selected = "head"),
 
-    # Application title
-    titlePanel("Old Faithful Geyser Data"),
-
-    # Sidebar with a slider input for number of bins 
-    sidebarLayout(
-        sidebarPanel(
-
-            
-            # Input: Select a file ----
-            fileInput("file1", "Choose CSV File",
-                      multiple = FALSE,
-                      accept = c("text/csv",
-                                 "text/comma-separated-values,text/plain",
-                                 ".csv")),
-            
-            # Horizontal line ----
-            tags$hr(),
-            
-            # Input: Checkbox if file has header ----
-            checkboxInput("header", "Header", TRUE),
-            
-            # Input: Select separator ----
-            radioButtons("sep", "Separator",
-                         choices = c(Comma = ",",
-                                     Semicolon = ";",
-                                     Tab = "\t"),
-                         selected = ","),
-            
-            # Input: Select quotes ----
-            radioButtons("quote", "Quote",
-                         choices = c(None = "",
-                                     "Double Quote" = '"',
-                                     "Single Quote" = "'"),
-                         selected = '"'),
-            
-            # Horizontal line ----
-            tags$hr(),
-            
-            # Input: Select number of rows to display ----
-            radioButtons("disp", "Display",
-                         choices = c(Head = "head",
-                                     All = "all"),
-                         selected = "head")
-        ),
-
-        # Show a plot of the generated distribution
-        mainPanel(
-           plotOutput("distPlot"),
-           plotOutput("lmPlot"),
-           tableOutput("contents")
-        )
+      # Action button to model the data ----
+      actionButton("model", "Generate Linear Model")
+        
+    ),
+    
+    # Show a plot of the scatter plot and linear model, and text output for coefficients
+    mainPanel(
+      plotOutput("scatterPlot"),
+      plotOutput("lmPlot"),
+      verbatimTextOutput("modelInfo"),
+      tableOutput("contents")
     )
+  )
 )
 
-# Define server logic required to draw a histogram
+# Define server logic required to draw scatter plot and linear model
 server <- function(input, output) {
-
-    dataInput <- reactive({
-        req(input$file1)
-        
-        df <- read.csv(input$file1$datapath,
-                       header = input$header,
-                       sep = input$sep,
-                       quote = input$quote)
-        return(df)
+  
+  dataInput <- reactive({
+    req(input$file1)
+    
+    df <- read.csv(input$file1$datapath,
+                   header = input$header,
+                   sep = input$sep,
+                   quote = input$quote)
+    return(df)
+  })
+  
+  output$scatterPlot <- renderPlot({
+    df <- dataInput()
+    plot(df$x, df$y, xlab = "X", ylab = "Y", main = "Scatter Plot")
+  })
+  
+  observeEvent(input$model, {
+    output$lmPlot <- renderPlot({
+      df <- dataInput()
+      plot(df$x, df$y, xlab = "X", ylab = "Y", main = "Scatter Plot with Linear Model")
+      lm_model <- lm(y ~ x, data = df)
+      abline(lm_model, col = "red")
     })
     
-    # output$distPlot <- renderPlot({
-    #     # generate bins based on input$bins from ui.R
-    #     x    <- faithful[, 2]
-    #     bins <- seq(min(x), max(x), length.out = input$bins + 1)
-    #     print(bins)
-    #     # draw the histogram with the specified number of bins
-    #     hist(x, breaks = bins, col = 'darkgray', border = 'white')
-    # })
-    # 
-    
-    output$distPlot <- renderPlot({
-        plot(dataInput()$x,dataInput()$y)
+    output$modelInfo <- renderPrint({
+      df <- dataInput()
+      lm_model <- lm(y ~ x, data = df)
+      model_summary <- summary(lm_model)
+      slope <- model_summary$coefficients[2, 1]
+      intercept <- model_summary$coefficients[1, 1]
+      correlation <- cor(df$x, df$y)
+      list(
+        Slope = slope,
+        Intercept = intercept,
+        `Correlation Coefficient` = correlation
+      )
     })
-    
-    output$lmtPlot <- renderPlot({
-        plot(dataInput()$x,dataInput()$y)
-    })
-    
-    
-    output$contents <- renderTable({
-        
-        # input$file1 will be NULL initially. After the user selects
-        # and uploads a file, head of that data file by default,
-        # or all rows if selected, will be shown.
-        
-        
-        if(input$disp == "head") {
-            return(head(dataInput()))
-        }
-        else {
-            return(dataInput())
-        }
-        
-    })
-        
+  })
+  
+  output$contents <- renderTable({
+    if (input$disp == "head") {
+      return(head(dataInput()))
+    } else {
+      return(dataInput())
+    }
+  })
 }
 
 # Run the application 
